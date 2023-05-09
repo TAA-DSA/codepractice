df74fad0d57fe00c0ca4047725fbe6e2c8f1306b
Tasdeed <ayonaziz@gmail.com>
2023-05-09 16:52:31 -0400

    completed the mutation challenge

--- script.js
+++ script.js
@@ -53,4 +53,27 @@ console.log(getIndexToIns([3, 10, 5], 3)); //return 0
 console.log(getIndexToIns([2, 5, 10], 15)); //return 3
 
 //3. Problem
-Console.log("Mutation");
+console.log("Mutation");
+
+//Return true if the string in the first element of the array contains all
+// of the letters of the string in the second element of the array.
+//For example, ["hello", "Hello"], should return true because all of the letters
+//in the second string are present in the first, ignoring case.
+
+//The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+//Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+const mutation = (arr) => {
+  //change the case(lower or upper) of the string so it is easy to compare.
+  const secondElement = arr[1].toLowerCase();
+  const firstElement = arr[0].toLowerCase();
+  for (let i = 0; i < secondElement.length; i++) {
+    if (firstElement.indexOf(secondElement[i]) < 0) return false;
+  }
+  return true;
+};
+
+console.log(mutation(["hello", "hey"])); //return false
+console.log(mutation(["Alien", "Line"])); //return true
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])); //return true
