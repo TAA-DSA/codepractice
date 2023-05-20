163f7e04cf6deb47854ff9e31dc7b7b5f50ace81
Tasdeed <ayonaziz@gmail.com>
2023-05-20 16:25:19 -0400

    cash register problem

--- script.js
+++ script.js
@@ -713,3 +713,91 @@ const telephoneCheck = (str) => {
 console.log(telephoneCheck("555-555-5555")); //output true
 console.log(telephoneCheck("8oo-six427676")); // false
 console.log(telephoneCheck("1 (555) 555-5555")); // true
+
+console.log("***Cash Register***");
+
+const checkCashRegister = (price, cash, cid) => {
+  const UNIT_AMOUNT = {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    ONE: 1.0,
+    FIVE: 5.0,
+    TEN: 10.0,
+    TWENTY: 20.0,
+    "ONE HUNDRED": 100.0,
+  };
+  let totalCID = 0;
+  for (let element of cid) {
+    totalCID += element[1];
+  }
+  totalCID = totalCID.toFixed(2);
+  let changeToGive = cash - price;
+  const changeArray = [];
+  if (changeToGive > totalCID) {
+    return { status: "INSUFFICIENT_FUNDS", change: changeArray };
+  } else if (changeToGive.toFixed(2) === totalCID) {
+    return { status: "CLOSED", change: cid };
+  } else {
+    cid = cid.reverse();
+    for (let elem of cid) {
+      let temp = [elem[0], 0];
+      while (changeToGive >= UNIT_AMOUNT[elem[0]] && elem[1] > 0) {
+        temp[1] += UNIT_AMOUNT[elem[0]];
+        elem[1] -= UNIT_AMOUNT[elem[0]];
+        changeToGive -= UNIT_AMOUNT[elem[0]];
+        changeToGive = changeToGive.toFixed(2);
+      }
+      if (temp[1] > 0) {
+        changeArray.push(temp);
+      }
+    }
+  }
+  if (changeToGive > 0) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  }
+  return { status: "OPEN", change: changeArray };
+};
+// Example usage
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
+
+console.log(
+  checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
+
+console.log(
+  checkCashRegister(3.26, 100, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ])
+);
